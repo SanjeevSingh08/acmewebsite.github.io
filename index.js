26732db5db0c6c228f4c12ae12c47a2463f80506
Sanjeev Ranjan Singh <sanjeevranjan.singh08@gmail.com>
2022-12-23 14:22:11 +0530

    aa
    a

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-var i=document.querySelector(".nav-menu-items");
+var i = document.querySelector(".nav-menu-items");
 
-document.querySelector(".open").addEventListener("click",menu)
-function menu(){
+document.querySelector(".open").addEventListener("click", menu);
+function menu() {
+  i.className = "hamburger-menu";
+  document.querySelector(".menu").style.background = "#000";
+  var prop = "brightness(0) invert(1)";
 
-i.className="hamburger-menu"
-document.querySelector(".menu").style.background="#000";
-var prop="brightness(0) invert(1)";
-
-
-
-var element=document.querySelector(".section-main")
-const style = getComputedStyle(element);
-const margin= style.marginTop;
-document.querySelector(".hamburger-menu").style.top=margin;
-document.querySelector(".open").style.display="none";
-document.querySelector(".close").style.display="block";
-document.querySelector(".close").style.filter=prop;
-
-
-
-
+  var element = document.querySelector(".section-main");
+  const style = getComputedStyle(element);
+  const margin = style.marginTop;
+  document.querySelector(".hamburger-menu").style.top = margin;
+  document.querySelector(".open").style.display = "none";
+  document.querySelector(".close").style.display = "block";
+  document.querySelector(".close").style.filter = prop;
 }
 
-document.querySelector(".close").addEventListener("click",close)
-function close(){
-i.classList.add("nav-menu-items")
-i.classList.remove="hamburger-menu"
-document.querySelector(".open").style.display="block";
-document.querySelector(".close").style.display="none";
-document.querySelector(".menu").style.background="none";
+document.querySelector(".close").addEventListener("click", close);
+function close() {
+  i.classList.add("nav-menu-items");
+  i.classList.remove = "hamburger-menu";
+  document.querySelector(".open").style.display = "block";
+  document.querySelector(".close").style.display = "none";
+  document.querySelector(".menu").style.background = "none";
 
-console.log("done")
+  console.log("done");
 }
