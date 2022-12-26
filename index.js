a42d81d5ea09b7d2580305f6dbf0a2fe551c610a
Sanjeev Ranjan Singh <sanjeevranjan.singh08@gmail.com>
2022-12-26 10:55:38 +0530

    a

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var i = document.querySelector(".nav-menu-items");
-
+var z = document.querySelector(".nav-menu-items");
+var element = document.querySelector(".section-main");
+const style = getComputedStyle(element);
 document.querySelector(".open").addEventListener("click", menu);
 function menu() {
-  i.className = "hamburger-menu";
+  z.className = "hamburger-menu";
   document.querySelector(".menu").style.background = "#000";
   var prop = "brightness(0) invert(1)";
 
-  var element = document.querySelector(".section-main");
-  const style = getComputedStyle(element);
+
   const margin = style.marginTop;
   document.querySelector(".hamburger-menu").style.top = margin;
   document.querySelector(".open").style.display = "none";
@@ -17,55 +17,47 @@ function menu() {
 
 document.querySelector(".close").addEventListener("click", close);
 function close() {
-  i.classList.add("nav-menu-items");
-  i.classList.remove = "hamburger-menu";
+  z.classList.add("nav-menu-items");
+  z.classList.remove = "hamburger-menu";
   document.querySelector(".open").style.display = "block";
   document.querySelector(".close").style.display = "none";
   document.querySelector(".menu").style.background = "none";
-
- 
 }
 
+var slider = document.querySelectorAll(".info");
+var counter = 0;
+var k = slider.length;
 
+for (var i = 0; i < slider.length; i++) {
+  k = 100 * i;
+  l = k + "%";
 
-var slider=document.querySelectorAll(".info");
-var counter=0;
-var k=slider.length;
-
-
-
-
-for(var i=0;i<slider.length;i++){
-  k=100*i;
-  l=k+"%"
-  
-  slider[i].style.left=l;
+  slider[i].style.left = l;
 }
 
-const gonext=()=>{
-  counter++
-  sliding()
-  
-}
-const goprev= ()=>{
+const gonext = () => {
+  counter++;
+  sliding();
+};
+const goprev = () => {
   counter--;
-  sliding()
-}
+  sliding();
+};
 
-function sliding(){
-  for(var i=0;i<slider.length;i++){
-  console.log(counter)
-  if(counter===-4){
-    counter=0;
-  }
-  else if( counter===1){
-  counter=-3
-  }
- 
-    k=counter*100
-    l=k+"%"
-    j="translateX"+"("+l+")"
-    console.log(j)
-slider[i].style.transform=j
+function sliding() {
+  for (var i = 0; i < slider.length; i++) {
+    console.log(counter);
+    if (counter === -4) {
+      counter = 0;
+    } else if (counter === 1) {
+      counter = -3;
+    }
+
+    k = counter * 100;
+    l = k + "%";
+    j = "translateX" + "(" + l + ")";
+    console.log(j);
+    slider[i].style.transform = j;
+    
   }
 }
